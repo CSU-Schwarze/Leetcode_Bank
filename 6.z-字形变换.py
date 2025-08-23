8f7a50da2c05820d0257b478018852f41100a32d
CSU-Schwarze <2997361783@qq.com>
2025-08-23 23:10:27 +0800

    5

---
+++ 6.z-字形变换.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode.cn id=6 lang=python
+#
+# [6] Z 字形变换
+#
+
+# @lc code=start
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        
+# @lc code=end
+
